5a90bacd197094fb17141909a3b3ce32d7a0df7e
Fabio José <fabiojose@gmail.com>
2020-05-08 17:20:27 -0300

    :bug: Corrigir nome do fatjar
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ KAFKA_CONSUMER_GROUP='spring-kafka-dlt-ex' \
 KAFKA_BOOTSTRAP_SERVERS='localhost:9092' \
 SCHEMA_REGISTRY_URL='http://localhost:8081' \
 KAFKA_FAIL_WHEN_MISSING_TOPICS='false' \
-java -Dserver.port='38002' -jar target/app-spring-boot.ja
+java -Dserver.port='38002' -jar target/app-spring-boot.jar
